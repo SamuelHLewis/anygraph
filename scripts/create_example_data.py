22f285a2e192a31ed3a6b619de49850f1f520551
samuelhlewis <samuel.h.lewis@gmail.com>
2025-05-06 15:08:17 +0100

    Restructure repo to separate scripts and data

--- scripts/create_example_data.py
+++ scripts/create_example_data.py
@@ -1,3 +1,4 @@
+import os
 from sklearn.datasets import load_iris
 
 X, y = load_iris(return_X_y=True, as_frame=True)
@@ -5,4 +6,6 @@ df = X.join(y)
 df['target'] = df['target'].replace(0, 'setosa')
 df['target'] = df['target'].replace(1, 'versicolour')
 df['target'] = df['target'].replace(2, 'virginica')
-df.to_csv('example_data.csv', index=False)
+
+os.makedirs('data', exist_ok=True)
+df.to_csv('data/example_data.csv', index=False)
